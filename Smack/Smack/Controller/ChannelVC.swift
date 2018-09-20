e5952d4be98c7c31d93b9d6c63229b533299ca8d
Admin <admin@Mac-Admin.local>
2018-09-20 19:52:35 +0300

    set up libraries and created the first view

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Admin on 20.09.2018.
+//  Copyright © 2018 vocabularity. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Admin on 20.09.2018.
+//  Copyright © 2018 vocabularity. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+}
