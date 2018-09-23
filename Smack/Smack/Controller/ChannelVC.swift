edbebfa125eb6b2c3fd1e70be6aeebe1df96d4bb
Admin <admin@Mac-Admin.local>
2018-09-24 00:56:32 +0300

    end of lesson 082

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -8,9 +8,7 @@
 
 import UIKit
 
-class ChannelVC: UIViewController
-    //, UITableViewDelegate, UITableViewDataSource
-{
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
@@ -65,24 +63,24 @@ class ChannelVC: UIViewController
         
     }
     
-    //Funcs for table filling - have to write them after implementing UITableViewDelegate and UITableViewDataSource
-    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
-    //
-    //            let channel = MessageService.instance.channels[indexPath.row]
-    //            cell.configureCell(channel: channel)
-    //            return cell
-    //        } else {
-    //            return UITableViewCell()
-    //        }
-    //    }
-    //
-    //    func numberOfSections(in tableView: UITableView) -> Int {
-    //        return 1
-    //    }
-    //
-    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //        return MessageService.instance.channels.count
-    //    }
+//    Funcs for table filling - have to write them after implementing UITableViewDelegate and UITableViewDataSource
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+    
+                let channel = MessageService.instance.channels[indexPath.row]
+                cell.configureCell(channel: channel)
+                return cell
+            } else {
+                return UITableViewCell()
+            }
+        }
+    
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return MessageService.instance.channels.count
+        }
     
 }
