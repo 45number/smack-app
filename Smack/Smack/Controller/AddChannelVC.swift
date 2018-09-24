a8fa33f17bf6a1d0faa5f89e96a6ba73a810e6cc
Admin <admin@Mac-Admin.local>
2018-09-24 15:54:38 +0300

    end of lesson 084

--- Smack/Smack/Controller/AddChannelVC.swift
+++ Smack/Smack/Controller/AddChannelVC.swift
@@ -24,6 +24,13 @@ class AddChannelVC: UIViewController {
     
     //Actions
     @IBAction func createChannelPressed(_ sender: Any) {
+        guard let channelName = nameTxt.text, nameTxt.text != "" else {return}
+        guard let channelDesc = chanDesc.text else {return}
+        SocketService.instance.addChannel(channelName: channelName, channelDescription: channelDesc) { (success) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
     @IBAction func closedModalPressed(_ sender: Any) {
