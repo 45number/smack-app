57dd27bd5fa55183652d350e5be3d093e2a051d0
Admin <admin@Mac-Admin.local>
2018-09-24 19:03:21 +0300

    end of lesson 086

---
+++ Smack/Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Admin on 24.09.2018.
+//  Copyright © 2018 vocabularity. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
